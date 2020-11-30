fe8063e8023cd84baeff113438a618e732205c6b
Chris <43553799+JBMILHOUSE@users.noreply.github.com>
2020-11-30 13:07:36 -0500

    fazendo upload da imagem via comando post em uma api rest

--- TrabalhoFinal/Rest.swift
+++ TrabalhoFinal/Rest.swift
@@ -6,6 +6,37 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIImage {
+    func toBase64() -> String? {
+        guard let imageData = self.pngData() else { return nil }
+        return imageData.base64EncodedString(options: Data.Base64EncodingOptions.lineLength64Characters)
+    }
+}
+
+enum dadoErro{
+    case urlErro
+    case taskEror(error: Error)
+    case noResponse
+    case noData
+    case responseStatusCode(code: Int)
+    case invalidJSON
+}
+
+class respostaRest: Codable{
+      var success: Bool
+      var response: String
+      var responseDescription: String
+      var id: String
+      
+      init() {
+          self.success = false
+          self.response = ""
+          self.responseDescription = ""
+          self.id = "-1"
+      }
+}
 
 class Rest {
     
@@ -23,7 +54,7 @@ class Rest {
     private static let session = URLSession(configuration: configuration)
 
 
-    class func loadData(_ ra : String, onComplete: @escaping([DadosFoto]) -> Void, onError: @escaping(erroStatus)-> Void){
+    class func loadData(_ ra: String,_ descricao: String,_ img: UIImage, onComplete: @escaping(respostaRest) -> Void, onError: @escaping(dadoErro)-> Void){
         
         //var dados : [DadosFoto] = []
         
@@ -35,44 +66,71 @@ class Rest {
             onError(.urlErro)
             return
         }
-    
-        let dataTask = session.dataTask(with: url,
-          completionHandler:
-            { (data: Data?, response: URLResponse?,error: Error?) in
-                if error == nil {  // sem erro na task
-                    guard let response = response as? HTTPURLResponse else {
-                        print("erro de resposta")
-                        onError(.noResponse)
-                        return
-                    }
-                    if response.statusCode == 200 {
-                        guard let data = data else {
-                            print("erro de dados")
-                            return
-                    }
-                    print(data)
-                    do {
-                        let dados = try JSONDecoder().decode( [DadosFoto].self , from: data)
-                        print(dados[0].descricao)
-                        print(dados[0].id)
-                        // print(dados.img_64)
-                    
-                        onComplete(dados)
-                    
-                    }catch {
-                        print(error)
-                        onError(.invalidJson)
-                    }
-                
-                    }else{
-                        onError(.responseStatusCode(code: response.statusCode))
-                    }
-            
-                } else{
-                    onError(.taskErro(error: error!))
-                    print(error!)
-                }
         
-    })
-    dataTask.resume()
-  }}
+        // por problemas no servidor a foto tem que ser menor que 10Mb
+        let imgScale = img.jpegData(compressionQuality: 0.7)
+        // let foto = foto1.toBase64()
+        // cria um optional e ai tem que desempacotar formato png
+        let foto = imgScale?.base64EncodedString()
+        var json = [String:Any]()
+        json["ra"] = ra
+        json["descricao"] = descricao
+        json["foto"] = foto!   // tira o option da sring
+    
+        do {
+                   
+                   let data = try JSONSerialization.data(withJSONObject: json, options: [])
+                   var request = URLRequest(url: url)
+                   request.httpMethod = "POST"
+                   request.httpBody = data
+                   request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+                   request.addValue("application/json", forHTTPHeaderField: "Accept")
+               
+                   let dataTask = URLSession.shared.dataTask(with: request, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) in
+                
+                       if error == nil {  // sem erro na task
+                           guard let response = response as? HTTPURLResponse else {
+                           print("erro de resposta")
+                           onError(.noResponse)
+                           return
+                       }
+                           
+                           if response.statusCode == 200 {
+                               
+                               guard let data = data else {
+                                   print("erro de dados")
+                                   return
+                               }
+                               print(data)
+                               do {
+                                   let dadosR = try JSONDecoder().decode( respostaRest.self , from: data)
+                                   print(dadosR.response)
+                                   print(dadosR.id)
+                                   
+                                   onComplete(dadosR)
+                                   
+                               }catch {
+                                   print(error)
+                                   onError(.invalidJSON)
+                               }
+                               
+                           }else{
+                               onError(.responseStatusCode(code: response.statusCode))
+                               print("deu erro")
+                           }
+                           
+                       } else{
+                           onError(.taskEror(error: error!))
+                           print(error!)
+                       }
+                       
+                   })
+                       
+               dataTask.resume()
+               }
+               catch {
+                   print("erro ao enviar")
+               }
+               
+           }
+  }
--- TrabalhoFinal/ViewController.swift
+++ TrabalhoFinal/ViewController.swift
@@ -7,10 +7,14 @@
 
 import UIKit
 
+var vSpinner: UIView?
+
 class ViewController: UIViewController {
+    var resposta = respostaRest()
 
     @IBOutlet weak var ivImage: UIImageView!
     
+    @IBOutlet weak var btnUpload: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -22,5 +26,44 @@ class ViewController: UIViewController {
             ivImage in self.ivImage.image = ivImage
         }
     }
+    @IBAction func fazerUpload(_ sender: Any) {
+        
+        self.showSpinner(onView: self.view)
+        Rest.loadData("17289", "IOs Latoya", self.ivImage.image!, onComplete: {( respostaRest ) in
+            let resp = respostaRest
+            print(resp.id)
+            DispatchQueue.main.async {
+                print("inserido")
+            }
+            self.removeSpinner()
+        }, onError: {( dadoErro ) in self.removeSpinner()
+            print(dadoErro)
+        })
+    }
 }
+
+extension UIViewController {
+    
+    func showSpinner(onView : UIView){
+        let spinnerView = UIView.init(frame: onView.bounds)
+        spinnerView.backgroundColor = UIColor.init(red: 0.5, green: 0.5, blue: 0.5,alpha: 0.5)
+        let ai = UIActivityIndicatorView.init(style: .whiteLarge)
+        ai.startAnimating()
+        ai.center = spinnerView.center
+        
+        DispatchQueue.main.async {
+            spinnerView.addSubview(ai)
+            onView.addSubview(spinnerView)
+        }
+        vSpinner = spinnerView
+    }
+    
+    func removeSpinner(){
+        DispatchQueue.main.async {
+            vSpinner?.removeFromSuperview()
+            vSpinner = nil
+        }
+    }
+}
+
 
